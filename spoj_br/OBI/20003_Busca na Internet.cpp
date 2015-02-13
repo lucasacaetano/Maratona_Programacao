563735272e630fc6999b0b5311b64dd5fe1f396c
Lucas Amaral Caetano <lucas.a.caetano@hotmail.com>
2015-02-13 19:32:24 -0200

    adicionando problemas OBI

---
+++ spoj_br/OBI/20003_Busca na Internet.cpp
@@ -0,0 +1,14 @@
+//http://br.spoj.com/problems/JBUSCA12/
+//codigo / id:  JBUSCA12 /
+#include <iostream>
+
+using std::cin;
+using std::cout;
+
+int main (){
+    int t;
+    cin >> t;
+    cout << t*4 << "\n";
+
+    return 0;
+}
---
+++ spoj_br/OBI/3829_Quadrados.cpp
@@ -0,0 +1,15 @@
+//http://br.spoj.com/problems/QUADRAD2/
+//codigo/id: QUADRAD2 / 3829
+#include <iostream>
+
+using std::cin;
+using std::cout;
+
+int main(){
+    int N;
+
+    cin >> N;
+    cout << N*N;
+
+    return 0;
+}
