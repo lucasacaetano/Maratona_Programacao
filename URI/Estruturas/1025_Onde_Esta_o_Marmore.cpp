c366ca7174c41730507d4a79af6447f820fc128e
Lucas Caetano <lucas.a.caetano@hotmail.com>
2018-08-07 23:30:50 -0300

    Retrabalho do problema 1025
    	Utilização de ordenação e busca binária, diminuindo a complexidade do algoritmo.
    	modified:   URI/Estruturas/1025_Onde_Esta_o_Marmore.cpp
    	modified:   URI/Iniciante/README.md
    	modified:   URI/README.md

--- URI/Estruturas/1025_Onde_Esta_o_Marmore.cpp
+++ URI/Estruturas/1025_Onde_Esta_o_Marmore.cpp
@@ -1,34 +1,36 @@
 #include <iostream>
-#include <set>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
 int main() {
     int n,q,marmore, caso = 1;
+    pair< vector<int>::iterator, vector<int>::iterator > it;
     do{
         cin >> n >> q;
         if( n == 0 || q == 0)
             break;
 
-        multiset<int> conjuntoMarmores;
-        multiset<int>::iterator it;
+        vector<int> m;
         for( int i = 0; i < n; i++ ) {
             cin >> marmore;
-            conjuntoMarmores.insert( marmore );
+            m.push_back( marmore );
         }
+
+        sort(m.begin(), m.end());
 
         cout << "CASE# " << caso++ << ":\n";
         for( int i = 0; i < q; i++ ) {
             cin >> marmore;
-            it = conjuntoMarmores.find( marmore );
-
-            if( it == conjuntoMarmores.end() ) {
-                cout << marmore << " not found\n";
-            } else {
+            if( binary_search(m.begin(), m.end(), marmore ) ) {
+                it = equal_range( m.begin(), m.end(), marmore );
                 cout << marmore
                     << " found at "
-                    << ( distance(conjuntoMarmores.begin(), it) + 1 )
+                    << ( (it.first - m.begin()) + 1 )
                     << "\n";
+            } else {
+                cout << marmore << " not found\n";
             }
         }
 
--- URI/README.md
+++ URI/README.md
@@ -4,3 +4,33 @@
 Pasta destinada ao repositódio de problemas [URI Online Judge](https://www.urionlinejudge.com.br)
 
 [logo]: https://urionlinejudge.r.worldssl.net/judge/img/5.0/logo.130615.png?1452205133
+
+## Iniciante
+
+Pasta com os problemas categorizados como iniciantes no repositório
+
+### Destaques
+
+A maioria dos problemas nessa categoria não necessitam de muito conhecimento diferenciado e podem ser resolvidos tranquilamente.
+A tabela abaixo destacam alguns problemas específicos onde a utilização de bibliotecas específicas ou técnicas de programação facilitam sua resolução.
+
+
+Código|Título|Técnia
+---|---|---
+1161|[Tempo de um Evento][1061]|Biblioteca de tempo `<ctime>`
+1179|[Preenchimento de Vetor IV][1179]|Estrutura de dados (Lista)
+
+
+[1061]: https://www.urionlinejudge.com.br/judge/pt/problems/view/1061
+[1179]: https://www.urionlinejudge.com.br/judge/pt/problems/view/1179
+
+## Estruturas e Bibliotecas
+
+Fila, Pilhas, Ordenação, Mapas...
+
+Código|Título|Técnia
+---|---|---
+1025|[Onde está o Mármore?][1025]|Ordenação e Busca Binária`<algoritm>, sort(), binary_search, equal_range`
+
+
+[1025]: https://www.urionlinejudge.com.br/judge/pt/problems/view/1025
